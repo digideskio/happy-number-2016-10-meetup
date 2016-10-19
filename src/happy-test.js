5a7ce593a059aa1fc66692cbf56d5ae33e4028e4
ridermansb <ridermansb@gmail.com>
2016-10-19 21:26:03 -0200

    Add more tests

--- src/happy-test.js
+++ src/happy-test.js
@@ -6,8 +6,12 @@ test('10 should be happy', () => expect(happy(10)).toBeTruthy())
 
 test('100 should be happy', () => expect(happy(100)).toBeTruthy())
 
-test('19 should be happy', () => expect(happy(100)).toBeTruthy())
+test('19 should be happy', () => expect(happy(19)).toBeTruthy())
 
-test('20 should be happy', () => expect(happy(100)).toBeTruthy())
+test('20 should be unhappy', () => expect(happy(20)).toBeFalsy())
 
-test('4 should be happy', () => expect(happy(100)).toBeTruthy())
+test('4 should be unhappy', () => expect(happy(4)).toBeFalsy())
+
+test('490 should be happy', () => expect(happy(490)).toBeTruthy())
+
+test('492 should be unhappy', () => expect(happy(492)).toBeFalsy())
--- src/happy.js
+++ src/happy.js
@@ -1,3 +1,3 @@
-export default function happy(number) {
+export default function happy (number) {
   return false
 }
