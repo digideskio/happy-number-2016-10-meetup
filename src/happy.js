5180cddf748e3858e568edd36688f1181d1f5840
ridermansb <ridermansb@gmail.com>
2016-10-21 20:28:35 -0200

    First commit solution

--- src/happy.js
+++ src/happy.js
@@ -1,3 +1,15 @@
 export default function happy (number) {
-  return false
+
+  let numbers = Array.from(number.toString())
+  let sum = 0
+
+  for (let n in numbers) {
+    sum += numbers[n] * numbers[n]
+  }
+
+  if (numbers.length !== 1) {
+    return happy(sum)
+  }
+  return number===1
+
 }
